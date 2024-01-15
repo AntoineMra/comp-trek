0254b9b5b10626c2a10fc9f3a3d726549191af99
MARIONNEAU Antoine <antoine.marionneau@aprilmarine.com>
2024-01-15 11:57:47 +0100

    add CSS on basic legal pages

--- components/layout/AppFooter.vue
+++ components/layout/AppFooter.vue
@@ -1,19 +1,21 @@
 <template>
-  <footer class="footer p-10 bg-neutral text-neutral-content">
-    <nav>
-      <header class="footer-title">Ressources</header>
+  <footer class="footer md:grid flex flex-wrap p-10 text-neutral-content">
+    <nav class="mx-auto">
+      <header class="text-primary text-lg font-bold">Produits</header>
       <NuxtLink to="/cookies" class="link link-hover">Branding</NuxtLink>
       <NuxtLink to="/cookies" class="link link-hover">Design</NuxtLink>
       <NuxtLink to="/cookies" class="link link-hover">Marketing</NuxtLink>
       <NuxtLink to="/cookies" class="link link-hover">Advertisement</NuxtLink>
     </nav>
-    <nav>
-      <header class="footer-title">Company</header>
-      <NuxtLink to="/cookies" class="link link-hover">About us</NuxtLink>
-      <NuxtLink to="/cookies" class="link link-hover">Contact</NuxtLink>
+    <nav class="mx-auto">
+      <header class="text-primary text-lg font-bold">Contact</header>
+      <NuxtLink to="/cookies" class="link link-hover">A propos</NuxtLink>
+      <NuxtLink to="/cookies" class="link link-hover">Support</NuxtLink>
     </nav>
-    <nav>
-      <header class="footer-title">Informations légales</header>
+    <nav class="mx-auto">
+      <header class="text-primary text-lg font-bold">
+        Informations légales
+      </header>
       <NuxtLink to="/mentions-legales" class="link link-hover"
         >Mentions générales</NuxtLink
       >
@@ -22,5 +24,29 @@
       >
       <NuxtLink to="/affiliate" class="link link-hover">Affiliation</NuxtLink>
     </nav>
+    <form>
+      <header class="footer-title">Newsletter</header>
+      <fieldset class="form-control w-80">
+        <label class="label">
+          <span class="label-text">Enter your email address</span>
+        </label>
+        <div class="join">
+          <input
+            type="text"
+            placeholder="username@site.com"
+            class="input input-bordered join-item"
+          />
+          <button class="btn btn-primary join-item">S'abonner</button>
+        </div>
+      </fieldset>
+    </form>
   </footer>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.footer {
+  background-color: rgb(240, 242, 250);
+}
+</style>
--- pages/affiliate.vue
+++ pages/affiliate.vue
@@ -1,14 +1,18 @@
 <template>
-  <main>
-    <header class="bg-blue-500 text-white py-4">
-      <h1 class="text-2xl md:text-3xl lg:text-4xl">
+  <main class="mx-10 my-5 lg:mx-80 lg:my-10 md:glass md:rounded">
+    <header class="md:py-4 md:px-16">
+      <div class="text-sm breadcrumbs">
+        <ul>
+          <li><a class="link-primary">Légal</a></li>
+          <li>Affiliation</li>
+        </ul>
+      </div>
+      <h1 class="text-2xl md:text-3xl lg:text-4xl font-bold text-primary pt-6">
         Vous apportez un point de vue de qualité est notre mission
       </h1>
     </header>
 
-    <section
-      class="max-w-2xl mx-auto my-8 p-4 md:p-8 bg-white shadow-md rounded-md"
-    >
+    <section class="max-w-2xl mx-auto my-8 md:p-4 md:p-8">
       <p class="mb-4">
         L’utilisation des liens d’affiliations nous permet d’améliorer en
         continue nos analyses.
@@ -19,9 +23,9 @@
       </h2>
       <p class="mb-4">
         Un lien affilié est un lien spécial qui contient un identifiant unique,
-        permettant à notre organisation de gagner une commission lorsque les
-        utilisateurs cliquent sur ce lien et effectuent un achat ou une action
-        spécifique sur le site partenaire.
+        permettant de gagner une commission lorsque les utilisateurs cliquent
+        sur ce lien et effectuent un achat ou une action spécifique sur le site
+        partenaire.
       </p>
 
       <h2 class="text-xl md:text-2xl lg:text-3xl font-bold mb-2">
@@ -30,9 +34,13 @@
       <p class="mb-4">
         Nous sélectionnons soigneusement des partenaires et des produits que
         nous croyons être pertinents et de qualité. Lorsque vous cliquez sur nos
-        liens affiliés et effectuez un achat, cela peut générer une commission
-        pour notre organisation, ce qui soutient nos efforts dans la fourniture
-        de perspectives de qualité.
+        liens affiliés et effectuez un achat, cela peut générer une commission.
+        Ces commissions sont pour nous le moyen de garantir un site de qualité
+        et de continuer à vous proposer des analyses pertinentes.
+      </p>
+      <p class="mb-4">
+        Fournir des liens d’affiliation est un service pour nos utilisateurs et
+        permet de développer une relation équitable pour tous.
       </p>
 
       <h2 class="text-xl md:text-2xl lg:text-3xl font-bold mb-2">
